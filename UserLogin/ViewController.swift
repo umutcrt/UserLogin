4bab9d5a1304e42a8c548cca5d63e6d67b07edd0
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-02-08 21:49:35 +0300

    user login

--- UserLogin/ViewController.swift
+++ UserLogin/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var password1Input: UITextField!
+    @IBOutlet weak var password2Input: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func signupButton(_ sender: Any) {
+        func alertFunc(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.destructive, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil) }
+        if password1Input.text == "" {
+            alertFunc(titleInput: "Error", messageInput: "Check and re-enter the information.")
+        }
+        else if usernameInput.text == "" {
+            alertFunc(titleInput: "Error", messageInput: "Check and re-enter the information.")
+        }
+        else if password1Input.text == password2Input.text {
+            alertFunc(titleInput: "Congratulations", messageInput: "Registration Successful")
+        }
+        else {
+            alertFunc(titleInput: "Error", messageInput: "Check and re-enter the information.")
+        }
+    }
 }
 
